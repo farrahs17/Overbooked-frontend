a27174c223c2c41e3495bed057d0ae760ac2c21f
farrahs17 <farrah.shawky.95@gmail.com>
2019-07-08 16:45:21 +0200

    fixing structure

--- reddit-proj/routes/post.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const postControllers = require("../controllers/postControllers");
-
-router.get("/posts", postControllers.getPosts);
-router.post("/post", postControllers.createPost);
-router.post("/comment", postControllers.addComment);
-router.post("/upvote", postControllers.addVotes);
-router.post("/downvote", postControllers.downVotes);
-module.exports = router;
--- reddit-proj/routes/user.js
+++
@@ -1,6 +0,0 @@
-const router = require("express").Router();
-const userControllers = require("../controllers/userController");
-
-router.post("/signup", userControllers.signUp);
-router.post("/login", userControllers.login);
-module.exports = router;
