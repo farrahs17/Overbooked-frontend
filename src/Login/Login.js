3dfc0475f5fd121a2be40e2a7015efa2248aca84
farrahs17 <farrah.shawky.95@gmail.com>
2019-07-13 18:06:12 +0200

    header styling and login

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,10 +1,12 @@
 import React from "react";
 import axios from "axios";
 import { saveToken } from "../Utils/utils";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 class Login extends React.Component {
   state = {
-    username: "",
+    email: "",
     password: ""
   };
 
@@ -12,7 +14,7 @@ class Login extends React.Component {
     e.preventDefault();
     axios
       .post("http://localhost:8080/api/login", {
-        username: this.state.username,
+        email: this.state.email,
         password: this.state.password
       })
       .then(res => {
@@ -22,9 +24,9 @@ class Login extends React.Component {
       });
   };
 
-  handleUsernameChange = e => {
-    const username = e.target.value;
-    this.setState({ username: username });
+  handleEmailChange = e => {
+    const email = e.target.value;
+    this.setState({ email: email });
   };
   handlePasswordChange = e => {
     const pass = e.target.value;
@@ -33,31 +35,56 @@ class Login extends React.Component {
 
   render() {
     return (
-      <div className="login-container">
-        <form onSubmit={this.handleSubmit}>
-          <label className="username-label">
-            Username
-            <input
-              onChange={this.handleUsernameChange}
-              type="text"
-              id="username"
-              name="username"
-            />
-          </label>
-          <label>
-            Password
-            <input
-              type="password"
-              id="password"
-              name="password"
-              onChange={this.handlePasswordChange}
-            />
-          </label>
-          <button onSubmit={this.handleSubmit} type="submit">
-            Log in
-          </button>
-        </form>
-      </div>
+      // <div className="login-container">
+      //   <form onSubmit={this.handleSubmit}>
+      //     <label className="username-label">
+      //       Username
+      //       <input
+      //         onChange={this.handleUsernameChange}
+      //         type="text"
+      //         id="username"
+      //         name="username"
+      //       />
+      //     </label>
+      //     <label>
+      //       Password
+      //       <input
+      //         type="password"
+      //         id="password"
+      //         name="password"
+      //         onChange={this.handlePasswordChange}
+      //       />
+      //     </label>
+      //     <button onSubmit={this.handleSubmit} type="submit">
+      //       Log in
+      //     </button>
+      //   </form>
+      // </div>
+
+      <Form onSubmit={this.handleSubmit}>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            onChange={this.handleEmailChange}
+            name="email"
+          />
+        </Form.Group>
+
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            onChange={this.handlePasswordChange}
+            name="password"
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Login
+        </Button>
+      </Form>
     );
   }
 }
